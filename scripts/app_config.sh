312e2655b6f6d86fff3a2597dc317bdb282b6b3d
Léo Guedes <leoguepe@gmail.com>
2023-12-29 14:42:14 -0300

    Corrigindo deploy na aws

--- scripts/app_config.sh
+++ scripts/app_config.sh
@@ -1,19 +1,6 @@
 #!/bin/bash
 
-if [ $environmentName == "production" || $environmentName == "development" ]; then
-    echo true
-else
-    environmentName=`curl -s http://169.254.169.254/latest/user-data | grep 'environment=' | cut -d '=' -f 2`
-    role=`curl -s http://169.254.169.254/latest/user-data | grep 'role=' | cut -d '=' -f 2`
-fi
-
-
-if [ $environmentName == "production" ]; then
-
-	s3baseUrl='s3://livrariadeploy/livraria/'
-else
-	s3baseUrl='s3://livrariadeploy/livraria/dev/'
-fi
+s3baseUrl='s3://livrariadeploy/livraria/'S
 
 rm -rf /var/www/html/livraria_old
 
